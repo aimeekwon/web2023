9770df6017a4557f5b1dfc4cbb0d020d39ec68d3
aimee <aimee00418@gmail.com>
2023-04-25 20:03:09 +0900

    0425
    00

---
+++ javascript/game/js/music.js
@@ -0,0 +1,54 @@
+const allMusic = [
+
+    {
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    },{
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    },{
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    },{
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    },{
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    },{
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    },{
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    },{
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    },{
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    },{
+        name : "1. hello",
+        artist : "patric",
+        img : "music_view01",
+        audio : "music_audio01.mp3" 
+    }
+]
